591c60f247d8727b9101f2fe3a635082993c63f4
Olaf Alders <admin@ww1.wundercounter.com>
2009-07-10 09:14:15 -0500

    Increased version number to 0.11 for release

--- lib/URI/ParseSearchString/More.pm
+++ lib/URI/ParseSearchString/More.pm
@@ -5,7 +5,7 @@ use strict;
 
 use base qw( URI::ParseSearchString );
 
-our $VERSION = '0.10';
+our $VERSION = '0.11';
 
 use CGI;
 use Data::Dumper;
@@ -402,7 +402,7 @@ URI::ParseSearchString::More - Extract search strings from more referrers.
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =head1 SYNOPSIS
 
